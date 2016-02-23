7308a2523ec1b816c4d4bd7086d80ddaa94621a8
Claudia Santana <civettesantana@gmail.com>
2016-02-22 20:36:54 -0700

    New tip file

---
+++ PracticeMethods.rb
@@ -0,0 +1,39 @@
+# Methods
+
+# def addition(num1, num2)
+# 	puts num1 + num2
+# end
+
+# def subtraction(num1, num2)
+# 	puts num1 - num2
+# end	
+
+# def say_name
+# 	puts "Jared"
+# end
+
+# def comparison(num1, num2)
+	# if num1 > num2
+	# 	puts "#{num1} is greatest :)"
+	# else
+	# 	puts "Else statement"
+	# end
+	
+# Ternary	
+
+	condition ? true : false
+num1 > num2 ? puts("#{num1} is greatest :)" : puts("Else statment")
+end
+
+
+comparison(8, 2)
+comparison(2, 8)
+
+
+# addition(2,6)
+# addition(3,4)
+# addition(8,10)
+
+# subtraction(10,5)
+
+# say_name
