61ba98645a3664807d1772bcc4003039fcaf7835
Claudia Santana <civettesantana@gmail.com>
2016-03-09 18:10:09 -0700

    New Folders in Repo

---
+++ Learnruby/online1.rb
@@ -0,0 +1,88 @@
+# PRACTICE 1
+
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+
+
+# PRACTICE 32 FOR LOOPS
+
+
+the_count = [1,2,3,4,5]
+# fruits = ['apple', 'oranges', 'pears', 'apricots']
+# change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+the_count.each do |number|
+	puts "This is count #{number}"
+end	
+
+# fruits.each do |fruit|
+# 	puts "A fruit of type: #{fruit}"
+# end
+
+# change.each {|i| puts "I got #{i}" }
+# elements = []
+
+# (0..5).each do |i|
+# 	puts "adding #{i} to the list."
+# 	elements << i
+# end
+
+# elements.each {|i| puts "Element was: #{i}"}
+
+# PRACTICE 33 WHILE LOOPS
+
+# i = 0
+# numbers = []
+
+# while i < 6
+# 	puts "At the top i is #{i}"
+# 	numbers.push(i)
+# 	i += 1
+# 	puts "Numbers now: ", numbers
+# 	puts "At the bottom i is #{i}"
+# end
+
+# puts "The Numbers:"
+
+# numbers.each {|num| puts num }
+
+
+# PRACTICE 34 ACCESSING ELEMENTS OF ARRAYS
+
+# animals = ['bear', 'tiger', 'penguin', 'zebra']
+# 	puts bear = animals[0]
+
+# animals = ['bear', 'ruby','peacock', 'kangaroo', 'whale', 'platypus']
+# 	bear = animals[0]
+# 	peacock = animals[2]
+# 	whale = animals[4]
+# 	ruby = animals [1]
+# 	platypus = animals[5]
+# 	kangaroo = animals[3]
+
+# 	puts bear
+# 	puts peacock
+# 	puts whale
+# 	puts ruby
+# 	puts platypus
+# 	puts kangaroo
+
+
+
+# animals = ["bear", "bird", "monkey"]
+
+# animals.each do |animal|
+# 		puts animal 
+
+# 	end	
+
+# PRACTICE 38 DOING THINGS TO ARRAYS
+
+# mystuff.push('hello')
+
+
--- week4_practice.rb
+++ week4_practice.rb
@@ -1,13 +1,31 @@
-def number(x)
-	x % 2 == 0 ? puts("Your are even") : puts("you are odd")
-end
-	# if x % 2 ==0
-	# 	puts "You are even"
-	# else 
-	# 	puts "You are odd"
-	# end	
-end
+# def number(x)
+# 	x % 2 == 0 ? puts("Your are even") : puts("you are odd")
+# end
+# 	# if x % 2 ==0
+# 	# 	puts "You are even"
+# 	# else 
+# 	# 	puts "You are odd"
+# 	# end	
+# end
 
-number(2)
-number(3)
+# number(2)
+# number(3)
+
+# Define a method that takes in one argument - array of integers
+# Within the method, loop through the array
+# If the number is less than 20, push into a new array
+# Display the new array
+
+# def number_check(array)
+# 	new_array= []
+
+# array.each do |x|
+# 	if x < 20
+# 		new_array.push(x)
+# 		end
+# 	end	
+# 	p new_array
+# end
+
+# number_check([1,2,3,4,5,6,7,30,78,98])
 
